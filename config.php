6f6c33ab7ab9796a20d9158951ae7b00ca3c2b70
saidsow <saiddsowma@gmail.com>
2024-02-04 02:04:00 -0500

    Update config.php

--- config.php
+++ config.php
@@ -1,24 +1,21 @@
 <?php
 	// Define the Gmail smtp server
-	define('MAILHOST', "smtp.hostinger.com");
+	define('MAILHOST', "smtp.gmail.com");
 	
 	// Define as a username the email that you use in your Gmail account.
-	define('USERNAME', "sds@saiddesignsolutions.com");
+	define('USERNAME', "saiddsowma@gmail.com");
 	
 	// Define your 16 digit Gmail app-password.
-	define('PASSWORD', "SaidDS92**");
+	define('PASSWORD', " fyjn btfh jeem oiiv");
 
 	// Define the email address from which the email is sent.
 	define('SEND_FROM', "sds@saiddesignsolutions.com");
 
 	// Define the name of the website from which the email is sent. 
-	define('SEND_FROM_NAME', "portfolio");
+	define('SEND_FROM_NAME', "Said");
 
 	// Define the reply-to address.
 	define('REPLY_TO', "sds@saiddesignsolutions.com");
 
 	// Define the reply-to name.
 	define('REPLY_TO_NAME', "Bob");
-
-
-	//fyjn btfh jeem oiiv
