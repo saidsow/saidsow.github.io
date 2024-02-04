72cde14fb4a1e2f9a3db3803cb933034e67c15ab
saidsow <saiddsowma@gmail.com>
2024-02-04 01:52:31 -0500

    email

--- config.php
+++ config.php
@@ -3,19 +3,19 @@
 	define('MAILHOST', "smtp.gmail.com");
 	
 	// Define as a username the email that you use in your Gmail account.
-	define('USERNAME', "YourEmail@gmail.com");
+	define('USERNAME', "saiddsowma@gmail.com");
 	
 	// Define your 16 digit Gmail app-password.
-	define('PASSWORD', "Your 16 digit Google app password");
+	define('PASSWORD', " fyjn btfh jeem oiiv");
 
 	// Define the email address from which the email is sent.
-	define('SEND_FROM', "email@your-domain.com");
+	define('SEND_FROM', "sds@saiddesignsolutions.com");
 
 	// Define the name of the website from which the email is sent. 
-	define('SEND_FROM_NAME', "My Website");
+	define('SEND_FROM_NAME', "Said");
 
 	// Define the reply-to address.
-	define('REPLY_TO', "email@your-domain.com");
+	define('REPLY_TO', "sds@saiddesignsolutions.com");
 
 	// Define the reply-to name.
-	define('REPLY_TO_NAME', "Your name");
+	define('REPLY_TO_NAME', "Bob");
