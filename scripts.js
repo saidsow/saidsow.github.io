c3ce3050cf2b1f03fd602c77e65e1dbef241b60d
Said Sowma <saiddsowma@gmail.com>
2024-04-01 03:48:58 -0400

    update

---
+++ scripts.js
@@ -0,0 +1,13 @@
+document.getElementById('about-link').addEventListener('click', function(event) {
+    event.preventDefault();
+    document.getElementById('about').style.display = 'block';
+    document.getElementById('projects').style.display = 'none';
+    document.getElementById('home').style.display = 'none';
+  });
+  
+  document.getElementById('projects-link').addEventListener('click', function(event) {
+    event.preventDefault();
+    document.getElementById('projects').style.display = 'block';
+    document.getElementById('about').style.display = 'none';
+    document.getElementById('home').style.display = 'none';
+  });
