f69f09efa3f6b700447db3232a40c97536cf6f82
Said Sowma <saiddsowma@gmail.com>
2024-06-29 04:03:34 -0400

    javascript

--- script.js
+++ script.js
@@ -19,8 +19,68 @@ $(document).ready(function() {
     });
 });
 
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
 
-gtag('config', 'G-6SQSM8S8LY');
+//about section
+document.addEventListener('DOMContentLoaded', () => {
+    const aboutCards = document.querySelectorAll('.about-card');
+
+    const observerOptions = {
+        root: null, // Use the viewport as the root
+        rootMargin: '0px',
+        threshold: 0.2 // Trigger when 20% of the element is in view
+    };
+
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.style.animation = 'slideright 1s forwards';
+            } else {
+                entry.target.style.animation = 'slideleft 1s forwards';
+            }
+        });
+    }, observerOptions);
+
+    aboutCards.forEach(card => {
+        observer.observe(card);
+    });
+});
+
+//Project section
+document.addEventListener('DOMContentLoaded', () => {
+    const elementsToAnimate = document.querySelectorAll('.project-card, .skill-slide, .service-card');
+
+    const observerOptions = {
+        root: null,
+        rootMargin: '0px',
+        threshold: 0.2 // Trigger when 20% of the element is in view
+    };
+
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                if (entry.target.classList.contains('project-card')) {
+                    entry.target.style.animation = 'slideIn 1s forwards';
+                } else if (entry.target.classList.contains('skill-slide')) {
+                    entry.target.style.animation = 'skill-slide 1s forwards';
+                } else if (entry.target.classList.contains('service-card')) {
+                    entry.target.style.animation = 'autoShow 1s forwards';
+                }
+            } else {
+                if (entry.target.classList.contains('project-card')) {
+                    entry.target.style.animation = 'none';
+                    entry.target.style.opacity = 0;
+                } else if (entry.target.classList.contains('skill-slide')) {
+                    entry.target.style.animation = 'none';
+                    entry.target.style.opacity = 0;
+                } else if (entry.target.classList.contains('service-card')) {
+                    entry.target.style.animation = 'none';
+                    entry.target.style.opacity = 0;
+                }
+            }
+        });
+    }, observerOptions);
+
+    elementsToAnimate.forEach(element => {
+        observer.observe(element);
+    });
+});
